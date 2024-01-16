7403d503aa542ea7d616e50ce96558b355f02ab3
Anushka Mittal <138426011+anushkamittal2001@users.noreply.github.com>
2024-01-16 17:09:42 +0530

    Update nctl.rb image update

--- nctl.rb
+++ nctl.rb
@@ -22,7 +22,7 @@ class Nctl < Formula
   end
 
   on_macos do
-    url "https://github.com/nirmata/go-nctl/releases/download/v0.0.0/nctl_0.0.0_darwin_all.zip", :using => NctlDownloadStrategy
+    url "https://nirmata-downloads.s3.us-east-2.amazonaws.com/nctl/nctl_3.4.0-rc2/nctl_3.4.0-rc2_macos_64-bit.zip", :using => NctlDownloadStrategy
     sha256 "2d9843a781152a32d9544118cb7fb50dd430c51bf6afc731d982e35e8581d7bc"
 
     def install
